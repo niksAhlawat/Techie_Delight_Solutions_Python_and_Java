2f9d15808f5ef797f0ad2846189f11b8b3ee654d
niksAhlawat <nikhilahlawat.nikk@outlook.com>
2021-12-18 22:43:38 +0530

    Find Pairs

--- Practice/Find_Pairs/Find_Pairs.java
+++ Practice/Find_Pairs/Find_Pairs.java
@@ -1,4 +1,23 @@
-package Practice;
+/** 
+
+Given an unsorted integer array, find a pair with the given sum in it.
+
+• Each input can have multiple solutions. The output should match with either one of them.
+
+Input : nums[] = [8, 7, 2, 5, 3, 1], target = 10
+Output: (8, 2) or (7, 3)
+
+• The solution can return pair in any order. If no pair with the given sum exists, the solution should return an empty tuple.
+
+Input : nums[] = [5, 2, 6, 8, 1, 9], target = 12
+Output: ()
+
+*/
+
+
+
+
+package Practice.Find_Pairs;
 
 import java.util.HashMap;
 import java.util.Map;
